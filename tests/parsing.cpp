2e3243301d6a91bfe05488dec4c7f3ce48e753b8
xaizek <xaizek@openmailbox.org>
2017-05-29 17:11:01 +0300

    Better tags for parsing tests

--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -3,13 +3,13 @@
 #include "TreeBuilder.hpp"
 #include "parser.hpp"
 
-TEST_CASE("Top-level macros are parsed successfully", "[parser]")
+TEST_CASE("Top-level macros are parsed successfully", "[parser][extensions]")
 {
     CHECK_FALSE(parse("TSTATIC_DEFS(int func(type *var);)").hasFailed());
     CHECK_FALSE(parse("ARRAY_GUARD(sort_enum, 1 + SK_COUNT);").hasFailed());
 }
 
-TEST_CASE("Empty initializer list is parsed", "[parser]")
+TEST_CASE("Empty initializer list is parsed", "[parser][extensions]")
 {
     CHECK_FALSE(parse("args_t args = {};").hasFailed());
 }
