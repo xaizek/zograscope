a9581dcf135abec4fa28eae5c72c2e828ddd1888
xaizek <xaizek@openmailbox.org>
2017-06-10 14:43:58 +0300

    Enable processing of empty files
    Here empty also means files that consist entirely of whitespace.
    
    C11 grammar doesn't allow this, but compilers do.  This also simplifies
    processing on higher levels.

--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -3,6 +3,13 @@
 #include "TreeBuilder.hpp"
 #include "parser.hpp"
 
+TEST_CASE("Empty input is OK", "[parser][extensions]")
+{
+    CHECK_FALSE(parse("").hasFailed());
+    CHECK_FALSE(parse("      ").hasFailed());
+    CHECK_FALSE(parse("\t\n \t \n").hasFailed());
+}
+
 TEST_CASE("Top-level macros are parsed successfully", "[parser][extensions]")
 {
     CHECK_FALSE(parse("TSTATIC_DEFS(int func(type *var);)").hasFailed());
