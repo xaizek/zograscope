ff53d4c5ae5b4614a5c5e9e7a4d66bd842d3ca5a
xaizek <xaizek@openmailbox.org>
2017-06-10 14:24:11 +0300

    Support type wrapped in a macro in C grammar
    This adds LOTS of new conflicts...

--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -7,6 +7,7 @@ TEST_CASE("Top-level macros are parsed successfully", "[parser][extensions]")
 {
     CHECK_FALSE(parse("TSTATIC_DEFS(int func(type *var);)").hasFailed());
     CHECK_FALSE(parse("ARRAY_GUARD(sort_enum, 1 + SK_COUNT);").hasFailed());
+    CHECK_FALSE(parse("XX(state_t) st;").hasFailed());
 }
 
 TEST_CASE("Empty initializer list is parsed", "[parser][extensions]")
