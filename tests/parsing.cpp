9087daf9960dfd2d9863c0ede025795c3257bbe6
xaizek <xaizek@openmailbox.org>
2017-07-12 12:24:47 +0300

    Parse top-level macros with empty argument list

--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -18,6 +18,7 @@ TEST_CASE("Top-level macros are parsed successfully", "[parser][extensions]")
     CHECK_FALSE(parse("TSTATIC_DEFS(int func(type *var);)").hasFailed());
     CHECK_FALSE(parse("ARRAY_GUARD(sort_enum, 1 + SK_COUNT);").hasFailed());
     CHECK_FALSE(parse("XX(state_t) st;").hasFailed());
+    CHECK_FALSE(parse("MACRO();").hasFailed());
 }
 
 TEST_CASE("Empty initializer list is parsed", "[parser][extensions]")
