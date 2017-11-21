xaizek/zograscope

commit c6ca2a5685f01b4b43359a47c23184d27ad5a8be

Levelize flattening
This makes comparison of expressions and conditions more reliable.

src/compare.cpp
@@ -11,6 +11,7 @@
 #include "tree-edit-distance.hpp"
 #include "utils.hpp"
 
+static bool flatten(Node *n, int level);
 static void markParents(Node *x, Node *parent);
 static void detectMoves(Node *x);
 static void refine(Node &node);
@@ -91,18 +92,18 @@ compare(Node *T1, Node *T2, TimeReport &tr, bool coarse, bool skipRefine)
         match.y->relative = match.x;
     }
 
-    // flatten unmatched trees into parent tree of their roots before doing
-    // common distilling
-    for (Node *&c : T1->children) {
-        if (canBeFlattened(T1, c) &&
-            c->relative == nullptr && c->next != nullptr && !c->next->last) {
-            c = c->next;
-        }
+    int flattenLevel = 0;
+    if (flatten(T1, flattenLevel) | flatten(T2, flattenLevel)) {
+        ++flattenLevel;
+        flatten(T1, flattenLevel);
+        flatten(T2, flattenLevel);
     }
-    for (Node *&c : T2->children) {
-        if (canBeFlattened(T2, c) &&
-            c->relative == nullptr && c->next != nullptr && !c->next->last) {
-            c = c->next;
+
+    // Flatten unmatched trees into parent tree of their roots before doing
+    // common distilling.
+    while (++flattenLevel < 3) {
+        if (flatten(T1, flattenLevel) | flatten(T2, flattenLevel)) {
+            break;
         }
     }
     distill(*T1, *T2);
@@ -129,6 +130,34 @@ compare(Node *T1, Node *T2, TimeReport &tr, bool coarse, bool skipRefine)
 
     visit(T1);
     refine(*T1);
+}
+
+static bool
+flatten(Node *n, int level)
+{
+    bool flattened = false;
+
+    for (Node *&c : n->children) {
+        if (c->next != nullptr && c->next->last) {
+            continue;
+        }
+
+        if (c->next == nullptr && !c->children.empty()) {
+            flattened |= flatten(c, level);
+            continue;
+        }
+
+        if (c->relative != nullptr) {
+            continue;
+        }
+
+        if (c->next != nullptr && canBeFlattened(n, c, level)) {
+            c = c->next;
+            flattened = true;
+        }
+    }
+
+    return flattened;
 }
 
 static void

src/tree.cpp
@@ -474,10 +474,22 @@ printSubTree(const Node &root)
 }
 
 bool
-canBeFlattened(const Node *parent, const Node *child)
+canBeFlattened(const Node *, const Node *child, int level)
 {
-    return child->stype != SType::Declaration
-        || parent->stype != SType::TranslationUnit;
+    switch (level) {
+        case 0:
+            return (child->stype == SType::IfExpr);
+
+        case 1:
+            return (child->stype == SType::ExprStatement);
+
+        default:
+            return child->stype != SType::Declaration
+                && child->stype != SType::ReturnValueStmt
+                && child->stype != SType::CallExpr
+                && child->stype != SType::Initializer
+                && child->stype != SType::Parameter;
+    }
 }
 
 bool
@@ -501,13 +513,6 @@ isContainer(const Node *x)
     return x->stype == SType::Statements
         || x->stype == SType::Bundle
         || x->stype == SType::BundleComma;
-}
-
-bool
-isExpr(const Node *x)
-{
-    return x->stype == SType::ExprStatement
-        || x->stype == SType::IfExpr;
 }
 
 bool

tests/diffing.cpp
@@ -146,31 +146,20 @@ TEST_CASE("All postponed nodes are pulled into parents",
 
 TEST_CASE("Reduced tree is compared correctly", "[comparison][reduction]")
 {
-    Tree oldTree = makeTree(R"(
-        void f()
-        {
-            if (variable < 2 || condition)
+    diffSources(R"(
+        void f() {
+            if (
+                variable < 2 ||  /// Deletions
+                condition
+               )
                 return;
         }
-    )", true);
-    Tree newTree = makeTree(R"(
-        void f()
-        {
+    )", R"(
+        void f() {
             if (condition)
                 return;
         }
     )", true);
-
-    TimeReport tr;
-    compare(oldTree.getRoot(), newTree.getRoot(), tr, true, true);
-
-    CHECK(countLeaves(*oldTree.getRoot(), State::Updated) == 0);
-    CHECK(countLeaves(*oldTree.getRoot(), State::Deleted) == 1);
-    CHECK(countLeaves(*oldTree.getRoot(), State::Inserted) == 0);
-
-    CHECK(countLeaves(*newTree.getRoot(), State::Updated) == 0);
-    CHECK(countLeaves(*newTree.getRoot(), State::Deleted) == 0);
-    CHECK(countLeaves(*newTree.getRoot(), State::Inserted) == 1);
 }
 
 TEST_CASE("Different trees are recognized as different", "[comparison]")
@@ -1445,12 +1434,12 @@ TEST_CASE("Matching parent value guides how leaves are matched", "[comparison]")
     )", false);
 }
 
-TEST_CASE("Expression is moved into if condition", "[comparison][moves]")
+TEST_CASE("Expressions are matched against if condition", "[comparison][moves]")
 {
-    // FIXME: semicolon shouldn't be marked as moved
     diffSources(R"(
         void f() {
-            magic_load(magic, NULL);     /// Moves
+            magic_load(magic, NULL)      /// Moves
+                ;                        /// Deletions
         }
     )", R"(
         void f() {
@@ -1460,6 +1449,58 @@ TEST_CASE("Expression is moved into if condition", "[comparison][moves]")
             { }                          /// Additions
         }
     )", false);
+
+    diffSources(R"(
+        void f() {
+            tabs_new_inner(&         /// Moves
+                           rwin      /// Updates
+                           )         /// Moves
+                           ;         /// Deletions
+        }
+    )", R"(
+        void f() {
+            if (                     /// Additions
+                tabs_new_inner(&     /// Moves
+                               lwin  /// Updates
+                               )     /// Moves
+                == NULL || 1) { }    /// Additions
+        }
+    )", true);
+
+    diffSources(R"(
+        void f() {
+            tabs_new_inner(&         /// Moves
+                           lwin      /// Moves
+                           )         /// Moves
+                           ;         /// Deletions
+        }
+    )", R"(
+        void f() {
+            if (                     /// Additions
+                tabs_new_inner(&     /// Moves
+                               lwin  /// Moves
+                               )     /// Moves
+                == NULL || 1) { }    /// Additions
+        }
+    )", true);
+
+    diffSources(R"(
+        void f() {
+            tabs_new_inner(&new_tab->left, &lwin)       /// Moves
+                ;                                       /// Deletions
+            tabs_new_inner(&new_tab->right, &rwin)      /// Moves
+                ;                                       /// Deletions
+        }
+    )", R"(
+        void f() {
+            if (                                        /// Additions
+                tabs_new_inner(&new_tab->left, &lwin)   /// Moves
+                == NULL ||                              /// Additions
+                tabs_new_inner(&new_tab->right, &rwin)  /// Moves
+                == NULL)                                /// Additions
+            { }                                         /// Additions
+        }
+    )", true);
 }
 
 TEST_CASE("Refining doesn't cause segfault", "[comparison][moves]")

src/change-distilling.cpp
@@ -52,8 +52,7 @@ canMatch(const Node *x, const Node *y)
         return false;
     }
 
-    if (xType == Type::Virtual && x->stype != y->stype &&
-        !(isExpr(x) && isExpr(y))) {
+    if (xType == Type::Virtual && x->stype != y->stype) {
         return false;
     }
 

src/tree.hpp
@@ -103,15 +103,13 @@ void reduceTreesCoarse(Node *T1, Node *T2);
 
 std::string printSubTree(const Node &root);
 
-bool canBeFlattened(const Node *parent, const Node *child);
+bool canBeFlattened(const Node *parent, const Node *child, int level);
 
 bool isUnmovable(const Node *x);
 
 bool hasMoveableItems(const Node *x);
 
 bool isContainer(const Node *x);
-
-bool isExpr(const Node *x);
 
 bool canForceLeafMatch(const Node *x, const Node *y);
 

commit d50e6444d108b2862fa433cb9a6e543c78321b03

Reduce regressions in expression matching
Caused by c6ca2a5685f01b4b43359a47c23184d27ad5a8be:

    Levelize flattening

    This makes comparison of expressions and conditions more
    reliable.

Which made it easier for expressions from different places to interact
with each other.  There are some more regressions caused by this change,
but due to nice effects on other things, it's not reverted.  Maybe we
can address them all.

src/c/c11-parser.ypp
@@ -1403,7 +1403,8 @@ expression_statement
     : ';'
     { $$ = tb->addNode($1, @1, SType::ExprStatement); }
     | expression ';'
-    { $$ = tb->addNode({ $1, tb->addNode($2, @2, SType::Separator) },
+    { $$ = tb->addNode({ tb->addNode($1, @2, SType::AnyExpression),
+                         tb->addNode($2, @2, SType::Separator) },
                        SType::ExprStatement); }
     ;
 
@@ -1415,7 +1416,7 @@ selection_statement
     : IF '(' expression ')' ext_statement %prec "then"
     { $$ = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                          tb->addNode($2, @2, SType::Separator),
-                         tb->addNode($3, @3, SType::IfCond),
+                         tb->addNode(tb->addNode($3, @3, SType::AnyExpression), @3, SType::IfCond),
                          tb->addNode($4, @4, SType::Separator),
                          tb->addNode($5, @5, SType::IfThen) },
                        SType::IfStmt); }
@@ -1425,7 +1426,7 @@ selection_statement
                                 SType::IfElse);
       $$ = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                          tb->addNode($2, @2, SType::Separator),
-                         tb->addNode($3, @3, SType::IfCond),
+                         tb->addNode(tb->addNode($3, @3, SType::AnyExpression), @3, SType::IfCond),
                          tb->addNode($4, @4, SType::Separator),
                          tb->addNode($5, @5, SType::IfThen),
                          ifElse },
@@ -1479,7 +1480,7 @@ iteration_statement
                                    tb->addNode($4, @4, SType::Separator),
                                    $5,
                                    tb->addNode($6, @6, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($7, @7, SType::Separator) },
                                  SType::ForHead);
       $$ = tb->addNode({ forHead, tb->addNode($8, @8) }, SType::ForStmt); }
@@ -1488,7 +1489,7 @@ iteration_statement
                                    tb->addNode($2, @2, SType::Separator),
                                    $3,
                                    tb->addNode($4, @4, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($5, @5, SType::Separator),
                                    $6,
                                    tb->addNode($7, @7, SType::Separator) },
@@ -1497,7 +1498,7 @@ iteration_statement
     | FOR '(' ';' expression ';' expression ')' ext_statement
     { auto forHead = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                                    tb->addNode($2, @2, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($3, @3, SType::Separator),
                                    $4,
                                    tb->addNode($5, @5, SType::Separator),
@@ -1508,9 +1509,9 @@ iteration_statement
     | FOR '(' ';' ';' expression ')' ext_statement
     { auto forHead = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                                    tb->addNode($2, @2, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($3, @3, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($4, @4, SType::Separator),
                                    $5,
                                    tb->addNode($6, @6, SType::Separator) },
@@ -1521,31 +1522,31 @@ iteration_statement
                                    tb->addNode($2, @2, SType::Separator),
                                    $3,
                                    tb->addNode($4, @4, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($5, @5, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($6, @6, SType::Separator) },
                                  SType::ForHead);
       $$ = tb->addNode({ forHead, tb->addNode($7, @7) }, SType::ForStmt); }
     | FOR '(' ';' expression ';' ')' ext_statement
     { auto forHead = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                                    tb->addNode($2, @2, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($3, @3, SType::Separator),
                                    $4,
                                    tb->addNode($5, @5, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($6, @6, SType::Separator) },
                                  SType::ForHead);
       $$ = tb->addNode({ forHead, tb->addNode($7, @7) }, SType::ForStmt); }
     | FOR '(' ';' ';' ')' ext_statement
     { auto forHead = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                                    tb->addNode($2, @2, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($3, @3, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($4, @4, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($5, @5, SType::Separator) },
                                  SType::ForHead);
       $$ = tb->addNode({ forHead, tb->addNode($6, @6) }, SType::ForStmt); }
@@ -1565,7 +1566,7 @@ iteration_statement
                                    $3,
                                    $4,
                                    tb->addNode($5, @5, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($6, @6, SType::Separator) },
                                  SType::ForHead);
       $$ = tb->addNode({ forHead, tb->addNode($7, @7) }, SType::ForStmt); }
@@ -1573,7 +1574,7 @@ iteration_statement
     { auto forHead = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                                    tb->addNode($2, @2, SType::Separator),
                                    $3,
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($4, @4, SType::Separator),
                                    $5,
                                    tb->addNode($6, @6, SType::Separator) },
@@ -1583,9 +1584,9 @@ iteration_statement
     { auto forHead = tb->addNode({ tb->addNode($1, @1, SType::Separator),
                                    tb->addNode($2, @2, SType::Separator),
                                    $3,
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($4, @4, SType::Separator),
-                                   tb->addNode({ }, SType::ExprStatement),
+                                   tb->addNode({ }, SType::AnyExpression),
                                    tb->addNode($5, @5, SType::Separator) },
                                  SType::ForHead);
       $$ = tb->addNode({ forHead, tb->addNode($6, @6) }, SType::ForStmt); }

src/compare.cpp
@@ -113,7 +113,7 @@ compare(Node *T1, Node *T2, TimeReport &tr, bool coarse, bool skipRefine)
 
     // Flatten unmatched trees into parent tree of their roots before doing
     // common distilling.
-    while (++flattenLevel < 3) {
+    while (++flattenLevel < 4) {
         if (flatten(T1, flattenLevel) | flatten(T2, flattenLevel)) {
             break;
         }

src/stypes.cpp
@@ -33,6 +33,7 @@ operator<<(std::ostream &os, SType stype)
         case SType::LabelStmt:           return (os << "LabelStmt");
         case SType::ForHead:             return (os << "ForHead");
         case SType::Expression:          return (os << "Expression");
+        case SType::AnyExpression:       return (os << "AnyExpression");
         case SType::Declarator:          return (os << "Declarator");
         case SType::Initializer:         return (os << "Initializer");
         case SType::InitializerList:     return (os << "InitializerList");

src/stypes.hpp
@@ -32,6 +32,7 @@ enum class SType : std::uint8_t
     LabelStmt,
     ForHead,
     Expression,
+    AnyExpression,
     Declarator,
     Initializer,
     InitializerList,

src/tree.cpp
@@ -197,6 +197,7 @@ isLayerBreak(SType stype)
         case SType::WhileCond:
         case SType::CallExpr:
         case SType::ExprStatement:
+        case SType::AnyExpression:
         case SType::ReturnValueStmt:
         case SType::Parameter:
         case SType::ForHead:
@@ -501,6 +502,9 @@ canBeFlattened(const Node *, const Node *child, int level)
 
         case 1:
             return (child->stype == SType::ExprStatement);
+
+        case 2:
+            return (child->stype == SType::AnyExpression);
 
         default:
             return child->stype != SType::Declaration

tests/diffing.cpp
@@ -1456,3 +1456,37 @@ TEST_CASE("Simple expressions are matched consistently", "[comparison]")
         }
     )", true);
 }
+
+TEST_CASE("Expressions aren't mixed with each other", "[comparison]")
+{
+    diffSources(R"(
+        void f() {
+            while(new_jobs != NULL) {
+                bg_job_t *const new_job = new_jobs;
+            }
+
+            while(*job != NULL) {
+                bg_job_t *const j = *job;
+
+                if(!j->running) {                          /// Deletions
+                    *job = j->err_next;                    /// Deletions
+                    pthread_spin_unlock(&j->status_lock);  /// Deletions
+                    continue;                              /// Deletions
+                }                                          /// Deletions
+            }
+        }
+    )", R"(
+        void f() {
+            while(*job != NULL) {                          /// Additions
+            }                                              /// Additions
+
+            while(new_jobs != NULL) {
+                bg_job_t *const new_job = new_jobs;
+            }
+
+            while(*job != NULL) {
+                bg_job_t *const j = *job;
+            }
+        }
+    )", true);
+}