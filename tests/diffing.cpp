xaizek/zograscope

commit 5f679b7ccb75b06488ef270f44e5f932033ddcf7

Make statements with matching bodies match
Despite mismatched values.

src/change-distilling.cpp
@@ -9,6 +9,8 @@
 #include "tree-edit-distance.hpp"
 #include "utils.hpp"
 
+static float childrenSimilarity(const Node *x, const std::vector<Node *> &po1,
+                                const Node *y, const std::vector<Node *> &po2);
 static bool isTerminal(const Node *n);
 
 namespace {
@@ -430,50 +432,8 @@ distill(Node &T1, Node &T2)
                     break;
                 }
 
-                NodeRange xChildren(po1, x), yChildren(po2, y);
-
-                int common = 0;
-                int yLeaves = 0;
-                for (const Node *n : yChildren) {
-                    if (!isTerminal(n)) {
-                        continue;
-                    }
-                    ++yLeaves;
-
-                    const Node *const parent = getParent(n);
-                    if (parent && parent->relative == nullptr) {
-                        // Skip children of unmatched internal nodes.
-                        continue;
-                    }
-
-                    if (n->relative == nullptr) {
-                        continue;
-                    }
-
-                    if (xChildren.includes(n->relative)) {
-                        ++common;
-                    }
-                }
-
-                int xLeaves = xChildren.terminalCount();
-
-                // Threshold of children similarity depends on number of leaves.
-                const float t = (std::min(xLeaves, yLeaves) <= 4 ? 0.4f : 0.6f);
-
-                const int xExtra = countSatelliteNodes(x);
-                const int yExtra = countSatelliteNodes(y);
-                common += std::min(xExtra, yExtra);
-                xLeaves += xExtra;
-                yLeaves += yExtra;
-
-                const int maxLeaves = std::max(xLeaves, yLeaves);
-                // Avoid NaN result.  If there are no common leaves, the nodes
-                // are the same (XXX: might want to compare satellites in such
-                // cases in the future).
-                const float childrenSim = maxLeaves == 0
-                                        ? 1.0f
-                                        : static_cast<float>(common)/maxLeaves;
-                if (childrenSim < t) {
+                const float childrenSim = childrenSimilarity(x, po1, y, po2);
+                if (childrenSim == 0.0f) {
                     continue;
                 }
 
@@ -582,6 +542,84 @@ distill(Node &T1, Node &T2)
             markNode(*y, State::Inserted);
         }
     }
+}
+
+// Computes children similarity.  Returns the similarity, which is 0.0 if it's
+// too small to consider nodes as matching.
+static float
+childrenSimilarity(const Node *x, const std::vector<Node *> &po1,
+                   const Node *y, const std::vector<Node *> &po2)
+{
+    NodeRange xChildren(po1, x), yChildren(po2, y);
+
+    NodeRange xValue, yValue;
+    if (haveValues(x, y)) {
+        xValue = NodeRange(po1, x->getValue());
+        yValue = NodeRange(po2, y->getValue());
+    }
+
+    // Number of common terminal nodes (terminals of
+    // unmatched internal nodes are not ignored).
+    int nonValueCommon = 0;
+    // Number of selected common terminal nodes (terminals of
+    // unmatched internal nodes are ignored).
+    int selCommon = 0;
+
+    int yLeaves = 0;
+    for (const Node *n : yChildren) {
+        if (!isTerminal(n)) {
+            continue;
+        }
+        ++yLeaves;
+
+        if (n->relative == nullptr || !xChildren.includes(n->relative)) {
+            continue;
+        }
+
+        if (!yValue.includes(n)) {
+            ++nonValueCommon;
+        }
+
+        const Node *const parent = getParent(n);
+        // This might skip children of unmatched internal nodes.
+        if (parent == nullptr || parent->relative != nullptr) {
+            ++selCommon;
+        }
+    }
+
+    int xLeaves = xChildren.terminalCount();
+
+    const int xExtra = countSatelliteNodes(x);
+    const int yExtra = countSatelliteNodes(y);
+    selCommon += std::min(xExtra, yExtra);
+    xLeaves += xExtra;
+    yLeaves += yExtra;
+
+    const int selMaxLeaves = std::max(xLeaves, yLeaves);
+    // Avoid NaN result.  If there are no common leaves, the nodes
+    // are the same (XXX: might want to compare satellites in such
+    // cases in the future).
+    const float childrenSim = selMaxLeaves == 0
+                            ? 1.0f
+                            : static_cast<float>(selCommon)/selMaxLeaves;
+
+    // Threshold of children similarity depends on number of leaves.
+    if (childrenSim >= (std::min(xLeaves, yLeaves) <= 4 ? 0.4f : 0.6f)) {
+        return childrenSim;
+    }
+
+    xLeaves -= xValue.terminalCount();
+    yLeaves -= yValue.terminalCount();
+
+    const int maxLeaves = std::max(xLeaves, yLeaves);
+    const float nonValueSim = maxLeaves == 0
+                            ? 1.0f
+                            : static_cast<float>(nonValueCommon)/maxLeaves;
+    if (nonValueSim >= 0.8) {
+        return nonValueSim;
+    }
+
+    return 0.0f;
 }
 
 // Checks whether node is leaf that "matters" (i.e., not a comment).

tests/diffing.cpp
@@ -1315,3 +1315,72 @@ TEST_CASE("Nodes with zero common non-satellite leaves are not marked updated",
     CHECK(countInternal(*newTree.getRoot(), SType::LabelStmt, State::Updated)
           == 0);
 }
+
+TEST_CASE("Head of while-loop is treated correctly", "[comparison]")
+{
+    diffSources(R"(
+        void f() {
+            while (new_jobs != NULL) {
+                new_job->err_next = *jobs;
+                *jobs = new_job;
+            }
+
+            while (*job != NULL) {          /// Moves
+                bg_job_t *const j = *job;   /// Moves
+            }                               /// Moves
+        }
+    )", R"(
+        void f() {
+            while (*job != NULL) {          /// Moves
+            }                               /// Moves
+
+            while (new_jobs != NULL) {
+                new_job->err_next = *jobs;
+                *jobs = new_job;
+            }
+
+            while (*job != NULL) {          /// Additions
+                bg_job_t *const j = *job;   /// Moves
+            }                               /// Additions
+        }
+    )", true);
+}
+
+TEST_CASE("Statements are matched by common bodies", "[comparison]")
+{
+    diffSources(R"(
+        void f() {
+            if (
+                !at_first_line(view)                               /// Deletions
+                ) {
+                new_pos = MIN(new_pos, view->list_pos - (int)offset);
+            }
+        }
+    )", R"(
+        void f() {
+            if (
+                fpos_can_move_up(view)                             /// Additions
+                ) {
+                new_pos = MIN(new_pos, view->list_pos - (int)offset);
+            }
+        }
+    )", true);
+
+    diffSources(R"(
+        void f() {
+            while (new_jobs != NULL)                   /// Deletions
+            {
+                int new_pos;
+                size_t offset = view->window_cells/2;
+            }
+        }
+    )", R"(
+        void f() {
+            while (veryDifferentCondition)             /// Additions
+            {
+                int new_pos;
+                size_t offset = view->window_cells/2;
+            }
+        }
+    )", true);
+}

src/tree.cpp
@@ -181,7 +181,8 @@ static bool
 isValueSNode(const SNode *n)
 {
     return n->value->stype == SType::FunctionDeclaration
-        || n->value->stype == SType::IfCond;
+        || n->value->stype == SType::IfCond
+        || n->value->stype == SType::WhileCond;
 }
 
 static bool
@@ -195,6 +196,7 @@ isLayerBreak(SType stype)
         case SType::Initializer:
         case SType::Declaration:
         case SType::IfCond:
+        case SType::WhileCond:
         case SType::CallExpr:
         case SType::ExprStatement:
         case SType::ReturnValueStmt:

commit 30fb6fb4249e8d5c6f017685fd7e7e1c47636d9f

Disregard values only if they aren't matched
Got broken by 5f679b7ccb75b06488ef270f44e5f932033ddcf7:

    Make statements with matching bodies match

    Despite mismatched values.

src/change-distilling.cpp
@@ -647,15 +647,19 @@ childrenSimilarity(const Node *x, const std::vector<Node *> &po1,
         return childrenSim;
     }
 
-    xLeaves -= xValue.terminalCount();
-    yLeaves -= yValue.terminalCount();
+    // Disregard values only if they aren't matched.
+    if (haveValues(x, y) && x->getValue()->relative == nullptr &&
+        y->getValue()->relative == nullptr) {
+        xLeaves -= xValue.terminalCount();
+        yLeaves -= yValue.terminalCount();
 
-    const int maxLeaves = std::max(xLeaves, yLeaves);
-    const float nonValueSim = maxLeaves == 0
-                            ? 1.0f
-                            : static_cast<float>(nonValueCommon)/maxLeaves;
-    if (nonValueSim >= 0.8) {
-        return nonValueSim;
+        const int maxLeaves = std::max(xLeaves, yLeaves);
+        const float nonValueSim = maxLeaves == 0
+                                ? 1.0f
+                                : static_cast<float>(nonValueCommon)/maxLeaves;
+        if (nonValueSim >= 0.8) {
+            return nonValueSim;
+        }
     }
 
     return 0.0f;

tests/diffing.cpp
@@ -1591,3 +1591,45 @@ TEST_CASE("Refining works for fine trees", "[comparison]")
 
     // Just checking that it doesn't crash.
 }
+
+TEST_CASE("Nested statements with values are matched correctly", "[comparison]")
+{
+    diffSources(R"(
+        void g() {
+            if(strcmp(part, "normal") == 0)         new_value |= SF_NORMAL;
+            else if(strcmp(part, "visual") == 0)    new_value |= SF_VISUAL;
+            else if(strcmp(part, "view") == 0)      new_value |= SF_VIEW;           /// Moves
+            else if(strcmp(part, "otherpane") == 0) new_value |= SF_OTHERPANE;      /// Moves
+            else if(strcmp(part, "keys") == 0)      new_value |= SF_KEYS;           /// Moves
+            else if(strcmp(part, "marks") == 0)     new_value |= SF_MARKS;          /// Moves
+            else if(starts_with_lit(part, "registers:")) {                          /// Moves
+                const char *const num = after_first(part, ':');                     /// Moves
+                new_value |= SF_REGISTERS;                                          /// Moves
+            } else if(strcmp(part, "delay") == 0)     new_value |= SF_DELAY;        /// Moves
+            else if(strcmp(part, "registers") == 0) new_value |= SF_REGISTERS;      /// Moves
+            else {                                                                  /// Moves
+                break_at(part, ':');                                                /// Moves
+                break;                                                              /// Moves
+            }                                                                       /// Moves
+        }
+    )", R"(
+        void g() {
+            if(strcmp(part, "normal") == 0)           new_value |= SF_NORMAL;
+            else if(strcmp(part, "visual") == 0)      new_value |= SF_VISUAL;
+            else if(strcmp(part, "foldsubkeys") == 0) new_value |= SF_FOLDSUBKEYS;  /// Additions
+            else if(strcmp(part, "view") == 0)        new_value |= SF_VIEW;         /// Moves
+            else if(strcmp(part, "otherpane") == 0)   new_value |= SF_OTHERPANE;    /// Moves
+            else if(strcmp(part, "keys") == 0)        new_value |= SF_KEYS;         /// Moves
+            else if(strcmp(part, "marks") == 0)       new_value |= SF_MARKS;        /// Moves
+            else if(starts_with_lit(part, "registers:")) {                          /// Moves
+                const char *const num = after_first(part, ':');                     /// Moves
+                new_value |= SF_REGISTERS;                                          /// Moves
+            } else if(strcmp(part, "delay") == 0)     new_value |= SF_DELAY;        /// Moves
+            else if(strcmp(part, "registers") == 0) new_value |= SF_REGISTERS;      /// Moves
+            else {                                                                  /// Moves
+                break_at(part, ':');                                                /// Moves
+                break;                                                              /// Moves
+            }                                                                       /// Moves
+        }
+    )", true);
+}