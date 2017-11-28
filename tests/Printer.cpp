xaizek/zograscope

commit a767e855d8086122dbf472c740a9a12854d038c3

Don't highlight lines that aren't displayed
It takes extra time.

There is more code now and it's more complicated, but the performance
gain in total for two files with 4500 lines is about 20%.  Time spent
on printing dropped by 50%.  So it's definitely worth it.

src/Highlighter.cpp
@@ -17,6 +17,8 @@
 
 #include "Highlighter.hpp"
 
+#include <algorithm>
+#include <limits>
 #include <sstream>
 #include <stack>
 #include <string>
@@ -42,14 +44,12 @@ static std::vector<boost::string_ref> toWords(const std::string &s);
 static std::string getSpelling(const Node &node, const decor::Decoration &dec,
                                bool original);
 
-namespace {
-
-class ColorPicker
+class Highlighter::ColorPicker
 {
 public:
     void setNode(const Node &node)
     {
-        prevNode = currNode;
+        prevNode = (currNode == &node ? nullptr : currNode);
         currNode = &node;
 
         prevHighlight = currHighlight;
@@ -90,95 +90,206 @@ private:
     const Node *prevNode = nullptr;
 };
 
+Highlighter::Highlighter(Node &root, bool original)
+    : line(1), col(1), colorPicker(new ColorPicker()), original(original)
+{
+    toProcess.push(&root);
 }
 
-Highlighter::Highlighter(bool original) : original(original)
+Highlighter::~Highlighter()
 {
+    // Emit destructor here with ColorPicker object being complete.
 }
 
 std::string
-Highlighter::print(Node &root) const
+Highlighter::print(int from, int n)
 {
-    std::ostringstream oss;
-    int line = 1, col = 1;
-    ColorPicker colorPicker;
-    std::vector<boost::string_ref> olines;
-    std::vector<boost::string_ref> lines;
-
-    std::stack<Node *> toProcess;
-    toProcess.push(&root);
-
-    while (!toProcess.empty()) {
-        Node *node = toProcess.top();
-        toProcess.pop();
-
-        if (node->next != nullptr) {
-            if (node->state != State::Unchanged) {
-                markTreeWithState(node->next, node->state);
-            }
-            if (node->moved) {
-                markTreeAsMoved(node->next);
-            }
-            node = node->next;
-        }
-
-        if (node->leaf) {
-            colorPicker.setNode(*node);
-
-            boost::optional<const decor::Decoration &> fillHighlight;
-            // Don't do filling across lines.
-            if (node->line == line) {
-                fillHighlight = colorPicker.getFillHighlight();
-                oss << *fillHighlight;
-            }
-
-            while (node->line > line) {
-                oss << '\n';
-                ++line;
-                colorPicker.advancedLine();
-                col = 1;
-            }
-
-            while (node->col > col) {
-                oss << ' ';
-                ++col;
-            }
-
-            if (fillHighlight) {
-                oss << decor::def;
-            }
-
-            const decor::Decoration &dec = colorPicker.getHighlight();
-
-            olines.clear();
-            lines.clear();
-
-            const std::string spelling = getSpelling(*node, dec, original);
-            split(node->spelling, '\n', olines);
-            split(spelling, '\n', lines);
-            oss << (dec << lines.front());
-            col += olines.front().size();
-
-            for (std::size_t i = 1U; i < lines.size(); ++i) {
-                std::size_t whitespaceLength =
-                    lines[i].find_first_not_of(" \t");
-                if (whitespaceLength == std::string::npos) {
-                    whitespaceLength = lines[i].size();
-                }
-                oss << '\n' << lines[i].substr(0, whitespaceLength)
-                            << (dec << lines[i].substr(whitespaceLength));
-                ++line;
-                col = 1 + olines[i].size();
-                colorPicker.advancedLine();
-            }
-        }
-
-        for (Node *child : boost::adaptors::reverse(node->children)) {
-            toProcess.push(child);
-        }
+    if (from < line) {
+        n = std::max(0, n - (line - from));
     }
 
-    return oss.str();
+    skipUntil(from);
+    print(n);
+
+    std::string result = oss.str();
+    oss.str(std::string());
+    return result;
+}
+
+std::string
+Highlighter::print()
+{
+    print(std::numeric_limits<int>::max());
+
+    std::string result = oss.str();
+    oss.str(std::string());
+    return result;
+}
+
+void
+Highlighter::skipUntil(int targetLine)
+{
+    if (line >= targetLine) {
+        return;
+    }
+
+    if (!lines.empty()) {
+        for (std::size_t i = 1U; i < lines.size(); ++i) {
+            if (++line == targetLine) {
+                olines.erase(olines.begin(), olines.begin() + i);
+                lines.erase(lines.begin(), lines.begin() + i);
+                return;
+            }
+        }
+        olines.clear();
+        lines.clear();
+    }
+
+    while (!toProcess.empty()) {
+        Node *const node = getNode();
+        if (!node->leaf) {
+            advanceNode(node);
+            continue;
+        }
+
+        colorPicker->setNode(*node);
+
+        while (node->line > line) {
+            if (++line == targetLine) {
+                return;
+            }
+        }
+
+        advanceNode(node);
+
+        split(node->spelling, '\n', olines);
+
+        for (std::size_t i = 1U; i < olines.size(); ++i) {
+            if (++line == targetLine) {
+                colorPicker->advancedLine();
+                const decor::Decoration &dec = colorPicker->getHighlight();
+                spelling = getSpelling(*node, dec, original);
+                split(spelling, '\n', lines);
+                olines.erase(olines.begin(), olines.begin() + i);
+                lines.erase(lines.begin(), lines.begin() + i);
+                return;
+            }
+        }
+
+        olines.clear();
+        lines.clear();
+    }
+}
+
+void
+Highlighter::print(int n)
+{
+    col = 1;
+    colorPicker->advancedLine();
+
+    if (!lines.empty()) {
+        printSpelling(n);
+    }
+
+    while (!toProcess.empty() && n != 0) {
+        Node *const node = getNode();
+        if (!node->leaf) {
+            advanceNode(node);
+            continue;
+        }
+
+        colorPicker->setNode(*node);
+
+        const decor::Decoration &fillHighlight =
+            colorPicker->getFillHighlight();
+        // Don't do filling across lines.
+        if (node->line == line && &fillHighlight != &decor::none) {
+            oss << fillHighlight;
+        }
+
+        while (node->line > line) {
+            ++line;
+            colorPicker->advancedLine();
+            if (--n == 0) {
+                return;
+            }
+            oss << '\n';
+            col = 1;
+        }
+
+        while (node->col > col) {
+            oss << ' ';
+            ++col;
+        }
+
+        if (&fillHighlight != &decor::none) {
+            oss << decor::def;
+        }
+
+        advanceNode(node);
+
+        spelling = getSpelling(*node, colorPicker->getHighlight(), original);
+        split(node->spelling, '\n', olines);
+        split(spelling, '\n', lines);
+
+        printSpelling(n);
+    }
+}
+
+void
+Highlighter::printSpelling(int &n)
+{
+    const decor::Decoration &dec = colorPicker->getHighlight();
+    oss << (dec << lines.front());
+    col += olines.front().size();
+
+    for (std::size_t i = 1U; i < lines.size(); ++i) {
+        ++line;
+        colorPicker->advancedLine();
+        if (--n == 0) {
+            olines.erase(olines.begin(), olines.begin() + i);
+            lines.erase(lines.begin(), lines.begin() + i);
+            return;
+        }
+
+        std::size_t whitespaceLength = lines[i].find_first_not_of(" \t");
+        if (whitespaceLength == std::string::npos) {
+            whitespaceLength = lines[i].size();
+        }
+        oss << '\n' << lines[i].substr(0, whitespaceLength)
+                    << (dec << lines[i].substr(whitespaceLength));
+        col = 1 + olines[i].size();
+    }
+
+    olines.clear();
+    lines.clear();
+}
+
+Node *
+Highlighter::getNode()
+{
+    Node *node = toProcess.top();
+
+    if (node->next != nullptr) {
+        if (node->state != State::Unchanged) {
+            markTreeWithState(node->next, node->state);
+        }
+        if (node->moved) {
+            markTreeAsMoved(node->next);
+        }
+        node = node->next;
+    }
+
+    return node;
+}
+
+void
+Highlighter::advanceNode(Node *node)
+{
+    toProcess.pop();
+    for (Node *child : boost::adaptors::reverse(node->children)) {
+        toProcess.push(child);
+    }
 }
 
 // Marks one layer of a tree specified by the node with the given state.

src/Highlighter.hpp
@@ -18,20 +18,48 @@
 #ifndef ZOGRASCOPE__HIGHLIGHTER_HPP__
 #define ZOGRASCOPE__HIGHLIGHTER_HPP__
 
+#include <memory>
+#include <sstream>
+#include <stack>
 #include <string>
+#include <vector>
+
+#include <boost/utility/string_ref.hpp>
 
 class Node;
 
 class Highlighter
 {
-public:
-    Highlighter(bool original = true);
+    class ColorPicker;
 
 public:
-    std::string print(Node &root) const;
+    Highlighter(Node &root, bool original = true);
+
+    Highlighter(const Highlighter&) = delete;
+    Highlighter & operator=(const Highlighter&) = delete;
+
+    ~Highlighter();
+
+public:
+    std::string print(int from, int n);
+    std::string print();
 
 private:
-    const bool original;
+    void skipUntil(int targetLine);
+    void print(int n);
+    void printSpelling(int &n);
+    Node * getNode();
+    void advanceNode(Node *node);
+
+private:
+    std::ostringstream oss;
+    int line, col;
+    std::unique_ptr<ColorPicker> colorPicker;
+    std::vector<boost::string_ref> olines;
+    std::vector<boost::string_ref> lines;
+    std::stack<Node *> toProcess;
+    std::string spelling;
+    bool original;
 };
 
 #endif // ZOGRASCOPE__HIGHLIGHTER_HPP__

src/Printer.cpp
@@ -169,16 +169,12 @@ Printer::print(TimeReport &tr)
     std::vector<DiffLine> diff = (tr.measure("compare"),
                                   makeDiff(std::move(lsrc), std::move(rsrc)));
 
-    // TODO: don't highlight lines that won't be displayed (it takes extra
-    //       time).
-    const std::string ls = (tr.measure("left-highlight"),
-                            Highlighter(true).print(left));
-    const std::string rs = (tr.measure("right-highlight"),
-                            Highlighter(false).print(right));
-    std::vector<boost::string_ref> l = split(ls, '\n');
-    std::vector<boost::string_ref> r = split(rs, '\n');
-
     auto timer = tr.measure("printing");
+
+    Highlighter lh(left, true);
+    Highlighter rh(right, false);
+    std::vector<std::string> l(lsrc.lines.size());
+    std::vector<std::string> r(rsrc.lines.size());
 
     unsigned int maxLeftWidth = 0U;
     unsigned int maxRightWidth = 0U;
@@ -197,20 +193,24 @@ Printer::print(TimeReport &tr)
             unsigned int width;
 
             case Diff::Left:
+                l[i] = lh.print(i + 1, 1);
                 width = measureWidth(l[i++]);
                 leftWidths.push_back(width);
                 maxLeftWidth = std::max(width, maxLeftWidth);
                 break;
             case Diff::Right:
+                r[j] = rh.print(j + 1, 1);
                 width = measureWidth(r[j++]);
                 maxRightWidth = std::max(width, maxRightWidth);
                 break;
             case Diff::Identical:
             case Diff::Different:
+                l[i] = lh.print(i + 1, 1);
                 width = measureWidth(l[i++]);
                 leftWidths.push_back(width);
                 maxLeftWidth = std::max(width, maxLeftWidth);
 
+                r[j] = rh.print(j + 1, 1);
                 width = measureWidth(r[j++]);
                 maxRightWidth = std::max(width, maxRightWidth);
                 break;

tests/Highlighter.cpp
@@ -33,6 +33,35 @@ TEST_CASE("Multiline tokens don't mess up positioning", "[highlighter]")
 
     Tree tree = makeTree(input);
 
-    std::string output = Highlighter().print(*tree.getRoot());
+    std::string output = Highlighter(*tree.getRoot()).print();
     CHECK(split(output, '\n') == split(input, '\n'));
+}
+
+TEST_CASE("Ranges are printed correctly", "[highlighter]")
+{
+    Tree tree = makeTree(
+R"(/* line1
+
+ * line3 */
+// line4
+/* line5
+ * line6
+ * line7 */
+// line8
+/* line9
+ * line10
+ * line11
+ * line12 */
+int f() { return 10; }
+// line14)", true);
+
+    Highlighter hi(*tree.getRoot());
+    CHECK(hi.print(1, 2) == "/* line1\n");
+    CHECK(hi.print(4, 2) == "// line4\n/* line5");
+    CHECK(hi.print(7, 1) == " * line7 */");
+    CHECK(hi.print(7, 1) == "");
+    CHECK(hi.print(6, 3) == "// line8");
+    CHECK(hi.print(10, 1) == " * line10");
+    CHECK(hi.print(14, 10) == "// line14");
+    CHECK(hi.print(20, 10) == "");
 }

tools/hi/hi.cpp
@@ -78,7 +78,7 @@ run(const CommonArgs &args, TimeReport &tr)
 
     dumpTree(args, tree);
     if (!args.dryRun) {
-        std::cout << Highlighter().print(*tree.getRoot()) << '\n';
+        std::cout << Highlighter(*tree.getRoot()).print() << '\n';
     }
     return EXIT_SUCCESS;
 }

commit 98a74ce5b9b7d86cd06cc1e5fdddea4a16cd2a79

Fix highlighting not skipping leading whitespace
Was just broken by a767e855d8086122dbf472c740a9a12854d038c3:

    Don't highlight lines that aren't displayed

src/Highlighter.cpp
@@ -91,7 +91,8 @@ private:
 };
 
 Highlighter::Highlighter(Node &root, bool original)
-    : line(1), col(1), colorPicker(new ColorPicker()), original(original)
+    : line(1), col(1), colorPicker(new ColorPicker()), original(original),
+      current(nullptr)
 {
     toProcess.push(&root);
 }
@@ -166,6 +167,7 @@ Highlighter::skipUntil(int targetLine)
 
         for (std::size_t i = 1U; i < olines.size(); ++i) {
             if (++line == targetLine) {
+                current = node;
                 colorPicker->advancedLine();
                 const decor::Decoration &dec = colorPicker->getHighlight();
                 spelling = getSpelling(*node, dec, original);
@@ -231,6 +233,7 @@ Highlighter::print(int n)
         spelling = getSpelling(*node, colorPicker->getHighlight(), original);
         split(node->spelling, '\n', olines);
         split(spelling, '\n', lines);
+        current = node;
 
         printSpelling(n);
     }
@@ -240,7 +243,22 @@ void
 Highlighter::printSpelling(int &n)
 {
     const decor::Decoration &dec = colorPicker->getHighlight();
-    oss << (dec << lines.front());
+
+    auto printLine = [&](boost::string_ref line) {
+        if (current->line == this->line) {
+            oss << (dec << lines.front());
+            return;
+        }
+
+        std::size_t whitespaceLength = line.find_first_not_of(" \t");
+        if (whitespaceLength == std::string::npos) {
+            whitespaceLength = line.size();
+        }
+        oss << line.substr(0, whitespaceLength)
+            << (dec << line.substr(whitespaceLength));
+    };
+
+    printLine(lines.front());
     col += olines.front().size();
 
     for (std::size_t i = 1U; i < lines.size(); ++i) {
@@ -252,12 +270,8 @@ Highlighter::printSpelling(int &n)
             return;
         }
 
-        std::size_t whitespaceLength = lines[i].find_first_not_of(" \t");
-        if (whitespaceLength == std::string::npos) {
-            whitespaceLength = lines[i].size();
-        }
-        oss << '\n' << lines[i].substr(0, whitespaceLength)
-                    << (dec << lines[i].substr(whitespaceLength));
+        oss << '\n';
+        printLine(lines[i]);
         col = 1 + olines[i].size();
     }
 

src/Highlighter.hpp
@@ -77,6 +77,7 @@ private:
     std::stack<Node *> toProcess;             // State of tree traversal.
     std::string spelling;                     // Storage behind `lines` field.
     bool original;                            // Whether this is an old version.
+    Node *current;                            // Node that's being processed.
 };
 
 #endif // ZOGRASCOPE__HIGHLIGHTER_HPP__

tests/Printer.cpp
@@ -248,6 +248,36 @@ TEST_CASE("Lines with changes aren't folded", "[printer]")
     REQUIRE(normalizeText(oss.str()) == expected);
 }
 
+TEST_CASE("Highlighting skips leading whitespace", "[printer]")
+{
+    Tree oldTree = makeTree("");
+    Tree newTree = makeTree(R"(
+        /* This
+         * is
+         * a
+         * comment */
+    )");
+
+    TimeReport tr;
+    compare(oldTree.getRoot(), newTree.getRoot(), tr, true, true);
+
+    std::ostringstream oss;
+    Printer printer(*oldTree.getRoot(), *newTree.getRoot(), oss);
+    printer.print(tr);
+
+    std::string expected = normalizeText(R"(
+        ~~~~~!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        ~~~~~!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         -   >  1
+         -   >  2          {+/* This+}
+         -   >  3           {+* is+}
+         -   >  4           {+* a+}
+         -   >  5           {+* comment */+}
+    )");
+
+    REQUIRE(normalizeText(oss.str()) == expected);
+}
+
 static std::string
 normalizeText(const std::string &s)
 {