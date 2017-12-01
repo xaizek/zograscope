b212d2acefd9e89031d1188efada5e73488f1b67
xaizek <xaizek@posteo.net>
2017-12-01 12:29:41 +0200

    Dispatch parsers through parser unit
    We need a single place to select one of several parsers.

---
+++ src/parser.cpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2017 xaizek <xaizek@posteo.net>
+//
+// This file is part of zograscope.
+//
+// zograscope is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// zograscope is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with zograscope.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "parser.hpp"
+
+#include <string>
+
+#include "c/c11-parser.hpp"
+#include "TreeBuilder.hpp"
+
+TreeBuilder
+parse(const std::string &contents, const std::string &fileName, bool debug,
+      cpp17::pmr::monolithic &mr)
+{
+    return c11_parse(contents, fileName, debug, mr);
+}
